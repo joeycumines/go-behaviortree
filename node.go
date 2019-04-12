0339ebc95b09131a3945b137f355f0697474096e
Joseph Cumines <joeycumines@gmail.com>
2019-04-12 21:14:22 +1000

    update license date

--- node.go
+++ node.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2018 Joseph Cumines
+   Copyright 2019 Joseph Cumines
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
- */
+*/
 
 // Package behaviortree provides a functional style implementation of behavior trees in Golang with no fluff
 package behaviortree
