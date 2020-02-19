af6091665bacc7544acf311ecb1d3aa2982d806d
Joseph Cumines <joeycumines@gmail.com>
2020-02-19 23:15:30 +1000

    update(license): increment date

--- selector_test.go
+++ selector_test.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019 Joseph Cumines
+   Copyright 2020 Joseph Cumines
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
